7d61a99e02a0d4423839a29f544ff14f523a8fcb
Irina Baldwin <2007inb@gmail.com>
2018-12-27 19:24:45 +0000

    Replace reject with scan

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,8 +1,7 @@
 class Isogram
 
   def self.isogram?(str)
-    chars = str.downcase.chars
-    alpha_chars = chars.reject { |char| char == " " || char == "-" }
+    alpha_chars = str.downcase.scan(/[a-z]/)
     pairs = alpha_chars.combination(2).to_a
     return pairs.any? { |a, b| a == b } ? false : true
   end

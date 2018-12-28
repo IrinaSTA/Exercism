5f9a27bb746ed170a5865108de529c44dc02864c
Irina Baldwin <2007inb@gmail.com>
2018-12-28 09:10:00 +0000

    Refactor for implicit true or false

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -2,7 +2,7 @@ class Isogram
 
   def self.isogram?(str)
     alpha_chars = str.downcase.scan(/[a-z]/)
-    true if alpha_chars == alpha_chars.uniq
+    alpha_chars == alpha_chars.uniq
   end
 
 end

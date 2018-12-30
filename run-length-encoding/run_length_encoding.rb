93fee7ec07b56e6f84e41d5550047253442d5181
Irina Baldwin <2007inb@gmail.com>
2018-12-30 23:09:29 +0000

    Solve, ugly implementation

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -2,6 +2,27 @@ class RunLengthEncoding
 
   def self.encode(input)
     output = ''
+    if !input.empty?
+      counter = 1
+      current_letter = input.chars.first
+      for char in input.chars[1..-1] do
+        if char == current_letter
+          counter += 1
+        else
+          if counter == 1
+            output << "#{current_letter}"
+          else
+            output << "#{counter}#{current_letter}"
+          end
+          if input.chars.last == char
+            output << "#{char}"
+          end
+          counter = 1
+          current_letter = char
+        end
+      end
+    end
+    output
   end
 
 end

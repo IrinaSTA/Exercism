9b2e50e77e4a1725447603bd3fff6baaffa0f121
Irina Baldwin <2007inb@gmail.com>
2018-12-30 23:51:22 +0000

    Refactor

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -1,20 +1,9 @@
 class RunLengthEncoding
 
   def self.encode(input)
-    blocks = []
-    while !input.empty? do
-      two_parts = self.partition_in_two(input)
-      blocks << two_parts[0]
-      input = two_parts[1]
-    end
-    blocks
     output = ''
-    for each in blocks do
-      if each.length == 1
-        output << "#{each[0]}"
-      else
-        output << "#{each.length}#{each[0]}"
-      end
+    for each in self.break_into_blocks(input) do
+      output << self.encode_block(each)
     end
     output
   end
@@ -26,6 +15,24 @@ class RunLengthEncoding
 
   def self.partition_in_two(input)
     input.partition(get_first_block(input))[1, 2]
+  end
+
+  def self.break_into_blocks(input)
+    blocks = []
+    while !input.empty? do
+      two_parts = self.partition_in_two(input)
+      blocks << two_parts[0]
+      input = two_parts[1]
+    end
+    blocks
+  end
+
+  def self.encode_block(block)
+    if block.length == 1
+      "#{block[0]}"
+    else
+      "#{block.length}#{block[0]}"
+    end
   end
 
 end

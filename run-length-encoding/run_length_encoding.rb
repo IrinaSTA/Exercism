8fb79dea7d329bf2bf4e87d0df88f78cea47c253
Irina Baldwin <2007inb@gmail.com>
2018-12-30 23:42:35 +0000

    Replace with better implementation
    - All encode tests passing

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -1,28 +1,39 @@
 class RunLengthEncoding
 
   def self.encode(input)
+    blocks = []
+    while !input.empty? do
+      two_parts = self.partition_in_two(input)
+      blocks << two_parts[0]
+      input = two_parts[1]
+    end
+    blocks
     output = ''
-    if !input.empty?
-      counter = 1
-      current_letter = input.chars.first
-      for char in input.chars[1..-1] do
-        if char == current_letter
-          counter += 1
-        else
-          if counter == 1
-            output << "#{current_letter}"
-          else
-            output << "#{counter}#{current_letter}"
-          end
-          if input.chars.last == char
-            output << "#{char}"
-          end
-          counter = 1
-          current_letter = char
-        end
+    for each in blocks do
+      if each.length == 1
+        output << "#{each[0]}"
+      else
+        output << "#{each.length}#{each[0]}"
       end
     end
     output
+  end
+
+# returns string of first block of letters in input string
+  def self.get_block(input)
+    first_letter = input.chars.first
+    input.scan(/\A#{first_letter}+/)[0]
+  end
+
+# partition strings into "", block and everything after the block
+# returns block and everything after the block
+  def self.partition_in_two(input)
+    input.partition(get_block(input))[1, 2]
+  end
+
+# returns rest of string
+  def self.return_to_do(input)
+    self.partition_in_two(input)[1]
   end
 
 end

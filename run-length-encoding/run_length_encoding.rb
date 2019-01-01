14b1e53b4178616fca448ee54b8d3b10225a1b75
Irina Baldwin <2007inb@gmail.com>
2019-01-01 15:55:45 +0000

    Add decode method
    - all tests passing; first pass solution complete
    - ugly implementation in need of refactoring

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -8,7 +8,18 @@ class RunLengthEncoding
     output
   end
 
+  def self.decode(input)
+    parts = self.break_into_parts(input)
+    output = ''
+    for each in parts do
+      output << self.decode_part(each)
+    end
+    output
+  end
+
   private
+
+  # encoding methods
 
   def self.break_into_blocks(input)
     blocks = []
@@ -20,6 +31,15 @@ class RunLengthEncoding
     blocks
   end
 
+  def self.partition_in_two(input)
+    input.partition(get_first_block(input))[1, 2]
+  end
+
+  def self.get_first_block(input)
+    first_letter = input.chars.first
+    input.scan(/\A#{first_letter}+/)[0]
+  end
+
   def self.encode_block(block)
     if block.length == 1
       "#{block[0]}"
@@ -28,13 +48,36 @@ class RunLengthEncoding
     end
   end
 
-  def self.get_first_block(input)
-    first_letter = input.chars.first
-    input.scan(/\A#{first_letter}+/)[0]
+  # decoding methods
+
+  def self.break_into_parts(input)
+    parts = []
+    while !input.empty? do
+      two_parts = self.partition_in_two_parts(input)
+      parts << two_parts[0]
+      input = two_parts[1]
+    end
+    parts
   end
 
-  def self.partition_in_two(input)
-    input.partition(get_first_block(input))[1, 2]
+  def self.decode_part(input)
+    output = ''
+    if input.length > 1
+      input.delete(input[-1]).to_i.times do
+        output << input[-1]
+      end
+    else
+      output << input[0]
+    end
+    output
+  end
+
+  def self.get_first_part(input)
+    input.scan(/\A(\d+\D|\D)/)[0][0]
+  end
+
+  def self.partition_in_two_parts(input)
+    input.partition(get_first_part(input))[1, 2]
   end
 
 end

98d37b69855bf64e57fbd2f65dab529555bb9d61
Irina Baldwin <2007inb@gmail.com>
2019-01-02 14:48:44 +0000

    Refactor

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -2,42 +2,29 @@ class RunLengthEncoding
 
   def self.encode(input)
     output = ''
-    for each in self.break_into_blocks(input) do
+    for each in self.break_into_blocks(input, __method__) do
       output << self.encode_block(each)
     end
     output
   end
 
   def self.decode(input)
-    parts = self.break_into_parts(input)
     output = ''
-    for each in parts do
-      output << self.decode_part(each)
+    for each in self.break_into_blocks(input, __method__) do
+      output << self.decode_block(each)
     end
     output
   end
 
   private
 
-  # encoding methods
-
-  def self.break_into_blocks(input)
+  def self.break_into_blocks(input, method)
     blocks = []
     while !input.empty? do
-      two_parts = self.partition_in_two(input)
-      blocks << two_parts[0]
-      input = two_parts[1]
+      blocks << self.get_current_block(input, method)
+      input = self.get_rest_of_string(input, method)
     end
     blocks
-  end
-
-  def self.partition_in_two(input)
-    input.partition(get_first_block(input))[1, 2]
-  end
-
-  def self.get_first_block(input)
-    first_letter = input.chars.first
-    input.scan(/\A#{first_letter}+/)[0]
   end
 
   def self.encode_block(block)
@@ -48,19 +35,7 @@ class RunLengthEncoding
     end
   end
 
-  # decoding methods
-
-  def self.break_into_parts(input)
-    parts = []
-    while !input.empty? do
-      two_parts = self.partition_in_two_parts(input)
-      parts << two_parts[0]
-      input = two_parts[1]
-    end
-    parts
-  end
-
-  def self.decode_part(input)
+  def self.decode_block(input)
     output = ''
     if input.length > 1
       input.delete(input[-1]).to_i.times do
@@ -72,12 +47,25 @@ class RunLengthEncoding
     output
   end
 
-  def self.get_first_part(input)
-    input.scan(/\A(\d+\D|\D)/)[0][0]
+  def self.get_current_block(input, method)
+    self.partition_in_two(input, method)[0]
   end
 
-  def self.partition_in_two_parts(input)
-    input.partition(get_first_part(input))[1, 2]
+  def self.get_rest_of_string(input, method)
+    self.partition_in_two(input, method)[1]
+  end
+
+  def self.partition_in_two(input, method)
+    input.partition(get_first_block(input, method))[1, 2]
+  end
+
+  def self.get_first_block(input, method)
+    if method == :decode
+      input.scan(/\A(\d+\D|\D)/)[0][0]
+    else
+      first_letter = input.chars.first
+      input.scan(/\A#{first_letter}+/)[0]
+    end
   end
 
 end

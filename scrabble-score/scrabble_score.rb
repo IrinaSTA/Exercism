c96a3757a275f7716b07bb87246b97866f92bbd4
Irina Baldwin <2007inb@gmail.com>
2019-01-04 09:53:36 +0000

    Refactor
    - use constant, not class variable
    - replace score_word with hash.fetch_values(splat).sum
    - remove dead code
    - replace initialize with more customary word || ""

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,6 +1,6 @@
 class Scrabble
 
-  @@dictionary = {
+  SCRABBLE_LETTER_VALUES = {
     "A"=>1, "B"=>3, "C"=>3, "D"=>2, "E"=>1, "F"=>4, "G"=>2, "H"=>4, "I"=>1,
     "J"=>8, "K"=>5, "L"=>1, "M"=>3, "N"=>1, "O"=>1, "P"=>3, "Q"=>10, "R"=>1,
     "S"=>1, "T"=>1, "U"=>1, "V"=>4, "W"=>4, "X"=>8, "Y"=>4, "Z"=>10, ""=>0
@@ -18,28 +18,16 @@ class Scrabble
 
   attr_reader :word
 
-  def initialize(word = nil)
-    @word = word.upcase if word
-  end
-
-  def dictionary
-    @@dictionary
+  def initialize(word)
+    @word = (word || "").upcase
   end
 
   def score_word
-    score = 0
-    for each in letters
-      score += score_letter(each)
-    end
-    score
+    SCRABBLE_LETTER_VALUES.fetch_values(*letters).sum
   end
 
   def letters
     word.scan(/[A-Z]/)
-  end
-
-  def score_letter(letter)
-    dictionary[letter]
   end
 
 end

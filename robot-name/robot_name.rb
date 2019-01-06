e2413b2940d945721262e99507617746eaddc5eb
Irina Baldwin <2007inb@gmail.com>
2019-01-06 21:15:39 +0000

    Initialize robot with a name

---
+++ robot-name/robot_name.rb
@@ -0,0 +1,27 @@
+class Robot
+
+  attr_reader :name
+
+  def initialize
+    @name = create_name
+  end
+
+  def self.forget
+  end
+
+  def create_name
+    random_letter + random_letter + "#{random_number}" + "#{random_number}" +
+    "#{random_number}"
+  end
+
+  LETTERS = ('A'..'Z').to_a
+
+  def random_letter
+    LETTERS[rand(LETTERS.count)]
+  end
+
+  def random_number
+    rand(9)
+  end
+
+end

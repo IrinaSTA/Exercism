c11018dbbb5d7e01425441a9213a722e801529f9
Irina Baldwin <2007inb@gmail.com>
2019-01-07 21:01:36 +0000

    Pass diff name when chose name is taken
    - Needs refactoring
    - Needs to pass the last test

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -1,30 +1,43 @@
 class Robot
 
+  @@taken_names = []
+
+  def self.taken_names
+    @@taken_names
+  end
+
   attr_reader :name
 
   def initialize
-    @name = create_name
+    @name = Robot.create_name
   end
 
   def reset
-    @name = create_name
+    @name = Robot.create_name
   end
 
   def self.forget
+    Robot.taken_names.clear
   end
-
-  def create_name
-    2.times.collect{random_letter}.join +
+# ugly implementation to pass the penultimate test
+  def self.create_name
+    name = 2.times.collect{random_letter}.join +
     3.times.collect{random_number}.join
+    if @@taken_names.include?(name)
+      name = 2.times.collect{random_letter}.join +
+      3.times.collect{random_number}.join
+    end
+    @@taken_names << name
+    name
   end
 
   LETTERS = ('A'..'Z').to_a
 
-  def random_letter
+  def self.random_letter
     LETTERS[rand(LETTERS.count)]
   end
 
-  def random_number
+  def self.random_number
     rand(9)
   end
 

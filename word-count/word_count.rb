854b03f9f7ff547716fac0bbc5adda87f822ab11
Irina Baldwin <2007inb@gmail.com>
2019-03-04 11:21:57 +0000

    Add solution to ruby word-count

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,13 +1,28 @@
 class Phrase
+
+  def word_count
+    word_count = {}
+    words.each do |word|
+      if word_count[word] == nil
+        word_count[word] = 1
+      else
+        word_count[word] += 1
+      end
+    end
+    word_count
+  end
+
+  private
+
   def initialize(phrase)
     @phrase = phrase
   end
-  def word_count
-    word_count = {}
-    @phrase.downcase.scan(/\w+'\w|\w+/).each do |word|
-      word_count[word] = 1 if word_count[word] == nil
-      word_count[word] += 1
-    end
-    return word_count
+
+  def phrase
+    @phrase
+  end
+
+  def words
+    phrase.downcase.scan(/\w+'\w|\w+/)
   end
 end

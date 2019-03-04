6412c6fa78335c226d370a1fa5f8376dd2d650cf
Irina Baldwin <2007inb@gmail.com>
2019-03-04 11:29:00 +0000

    Move method into instance; create class valid? helper

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,31 +1,40 @@
 class Luhn
 
+  def initialize(number)
+    @number = number
+  end
+
   def self.valid?(number)
+    luhn = Luhn.new(number)
+    luhn.valid?(number)
+  end
+
+  def valid?(number)
     return false unless self.valid_length?(number) &&
     self.all_valid_chars?(number)
 
     self.luhn_sum(number) % 10 == 0
   end
 
-  def self.valid_length?(number)
+  def valid_length?(number)
     number.scan(/\d/).length > 1
   end
 
-  def self.all_valid_chars?(number)
+  def all_valid_chars?(number)
     (number.scan(/\D/) - number.scan(/\s/)).empty?
   end
 
-  def self.luhn_sum(number)
+  def luhn_sum(number)
     number.
     reverse.
     scan(/\d/).
     map(&:to_i).
     map.with_index{
-      |num, index| index.odd? ? self.double_luhn_digit(num) : num
+      |num, index| index.odd? ? double_luhn_digit(num) : num
     }.sum
   end
 
-  def self.double_luhn_digit(digit)
+  def double_luhn_digit(digit)
     result = digit * 2
     result >= 10 ? result - 9 : result
   end
